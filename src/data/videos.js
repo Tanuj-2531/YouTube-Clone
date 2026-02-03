8af327287461f348e4dac0f065c71b8e380235bd
Tanuj Agarwal <tanujag.2531@gmail.com>
2026-02-04 00:05:36 +0530

    Added static video data with categories for filter functionality

---
+++ src/data/videos.js
@@ -0,0 +1,29 @@
+// Static sample videos for homepage display
+const videos = [
+  {
+    videoId: "video01",
+    title: "Learn React in 30 Minutes",
+    thumbnailUrl: "https://i.ytimg.com/vi/dGcsHMXbSOA/maxresdefault.jpg",
+    channelName: "Code Academy",
+    views: "15K views",
+    category: "React"
+  },
+  {
+    videoId: "video02",
+    title: "JavaScript Full Course",
+    thumbnailUrl: "https://i.ytimg.com/vi/W6NZfCO5SIk/maxresdefault.jpg",
+    channelName: "Dev Simplified",
+    views: "120K views",
+    category: "JavaScript"
+  },
+  {
+    videoId: "video03",
+    title: "Top 10 Gaming Tricks",
+    thumbnailUrl: "https://i.ytimg.com/vi/2g811Eo7K8U/maxresdefault.jpg",
+    channelName: "Pro Gamer",
+    views: "89K views",
+    category: "Gaming"
+  }
+];
+
+export default videos;
